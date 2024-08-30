3f79fbe383125fef358aab7e2cd4749098cea37a
das747 <40293014+das747@users.noreply.github.com>
2024-08-30 18:53:54 +0200

    inplace refactoring

---
+++ src/main/kotlin/com/das747/ebnfplugin/ide/EbnfRefactoringSupportProvider.kt
@@ -0,0 +1,14 @@
+package com.das747.ebnfplugin.ide
+
+import com.das747.ebnfplugin.lang.psi.EbnfRule
+import com.intellij.lang.refactoring.RefactoringSupportProvider
+import com.intellij.psi.PsiElement
+
+class EbnfRefactoringSupportProvider: RefactoringSupportProvider() {
+    override fun isMemberInplaceRenameAvailable(
+        element: PsiElement,
+        context: PsiElement?
+    ): Boolean {
+        return element is EbnfRule
+    }
+}
